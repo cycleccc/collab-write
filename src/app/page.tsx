18168b64bd27bf17fcbc07224b84d4261af5b0c6
yaolongfei <2991205548@qq.com>
2024-03-08 18:10:53 +0800

    feat: 添加默认字体字号

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 "use client"
-/**
- * @description React wangEditor usage
- * @author wangfupeng
- */
+import '@wangeditor/editor/dist/css/style.css' // 引入 css
 
-import React, { useState, useEffect } from "react";
-import "@wangeditor/editor/dist/css/style.css";
-import { Editor, Toolbar } from "@wangeditor/editor-for-react";
-
+import React, { useState, useEffect } from 'react'
+import { Editor, Toolbar } from '@wangeditor/editor-for-react'
+import { IDomEditor, IEditorConfig, IToolbarConfig } from '@wangeditor/editor'
+import Home from '@/component/layout/home'
 function MyEditor() {
-    const [editor, setEditor] = useState(null); // 存储 editor 实例
+    // editor 实例
+    const [editor, setEditor] = useState<IDomEditor | null>(null)   // TS 语法
+    // const [editor, setEditor] = useState(null)                   // JS 语法
+    console.log(editor?.getAllMenuKeys())
+    console.log(editor?.getMenuConfig('fontSize'))
+    // 编辑器内容
     const [html, setHtml] = useState("<p>hello</p>");
     const jsonContent = [
         {
@@ -22,64 +24,59 @@ function MyEditor() {
     ];
     // 模拟 ajax 请求，异步设置 html
     // useEffect(() => {
-    //   setTimeout(() => {
-    //     setHtml(
-    //       '<p style="line-height: 1.5;"><span style="font-size: 32px; font-family: 微软雅黑;">111111111111</span></p>'
-    //     );
-    //   }, 1500);
-    // }, []);
+    //     setTimeout(() => {
+    //         setHtml('<p>hello world</p>')
+    //     }, 1500)
+    // }, [])
 
-    const toolbarConfig = {};
-    const editorConfig = {
-        placeholder: "请输入内容...",
-    };
+    // 工具栏配置
+    const toolbarConfig: Partial<IToolbarConfig> = {}  // TS 语法
+    // const toolbarConfig = { }                        // JS 语法
 
-    // 及时销毁 editor
+    // 编辑器配置
+    const editorConfig: Partial<IEditorConfig> = {    // TS 语法
+        // const editorConfig = {                         // JS 语法
+        placeholder: '请输入内容...',
+    }
+
+    // 及时销毁 editor ，重要！
     useEffect(() => {
-      return () => {
-          if (editor == null) return;
-          editor.destroy();
-          setEditor(null);
-      };
-  }, [editor]);
-
-    function insertText() {
-        if (editor == null) return;
-        editor.insertText(" hello ");
-    }
-
-    function printHtml() {
-        if (editor == null) return;
-        console.log(editor.getHtml());
-    }
+        return () => {
+            if (editor == null) return
+            editor.destroy()
+            setEditor(null)
+        }
+    }, [editor])
 
     return (
         <>
-          <div>
-              <button onClick={ insertText }>insert text</button>
-              <button onClick={ printHtml }>print html</button>
-          </div>
-
-          <div style={ { border: "1px solid #ccc", zIndex: 100, marginTop: "15px" } }>
-              <Toolbar
-                  editor={ editor }
-                  defaultConfig={ toolbarConfig }
-                  mode="default"
-                  style={ { borderBottom: "1px solid #ccc" } }
-              />
-              <Editor
-                  defaultConfig={ editorConfig }
-                  value={ html }
-                  defaultContent={ jsonContent }
-                  onCreated={ setEditor }
-                  onChange={ (editor) => setHtml(editor.getHtml()) }
-                  mode="default"
-                  style={ { height: "500px" } }
-              />
-          </div>
-          <div style={ { marginTop: "15px" } }>{ html }</div>
-      </>
-  );
+            <div className='h-100  w-100 z-0'>
+                <Toolbar
+                    editor={ editor }
+                    defaultConfig={ toolbarConfig }
+                    mode="default"
+                    style={ { borderBottom: '1px solid #ccc' } }
+                />
+                <Editor
+                    defaultConfig={ editorConfig }
+                    value={ html }
+                    defaultContent={ jsonContent }
+                    onCreated={ setEditor }
+                    onChange={ editor => setHtml(editor.getHtml()) }
+                    mode="default"
+                    style={ { height: '500px', overflowY: 'hidden' } }
+                />
+            </div>
+            <div style={ { marginTop: '15px' } }>
+                { html }
+            </div>
+        </>
+    )
 }
-
-export default MyEditor;
+function home() {
+    return <div>
+        <Home />
+        <MyEditor />
+    </div>
+}
+export default home
