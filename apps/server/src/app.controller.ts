b002cb37c6ef260acd25765fef7135453b6ce7d2
yaolongfei <2991205548@qq.com>
2024-03-18 18:12:00 +0800

    test(init module): test fastify api

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Res, Req, } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RouteConfig, RouteConstraints } from '@nestjs/platform-fastify';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+    @RouteConfig({ output: 'hello world' })
+    @RouteConstraints({ version: '1.2.x' })
+    newFeature() {
+        return 'This works only for version >= 1.2.x';
+    }
     @Get()
-    index(@Res() res) {
-        res.status(302).redirect('/login');
+    // index(@Req() req) {
+    //     return req.routeConfig.output;
+    // }
+    getHello(): string {
+        return this.appService.getHello();
     }
 }
