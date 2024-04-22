64a56d8479fd5d047f103a9bf431b1c33ae7799c
yaolongfei <2991205548@qq.com>
2024-04-22 18:24:29 +0800

    style( eslint ): modify style by eslint

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,15 @@
-"use client"
+'use client'
 import '@wangeditor/editor/dist/css/style.css' // 引入 css
 
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { IDomEditor, IEditorConfig, IToolbarConfig, i18nGetResources, t } from '@wangeditor/editor'
 import Editor from '@/component/layout/home'
+
 function home() {
-    return <div>
-        <Editor />
+  return (
+    <div>
+      <Editor />
     </div>
+  )
 }
 export default home
