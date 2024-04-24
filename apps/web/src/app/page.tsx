b40d24eafd810974309badaefd52a73d4a900b99
yaolongfei <2991205548@qq.com>
2024-04-24 18:16:22 +0800

    feat(auth): add authentication-related components and logic

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,22 @@
 'use client'
-import '@wangeditor/editor/dist/css/style.css' // 引入 css
 
-import React from 'react'
+import type { ReactElement, ReactNode } from 'react'
 import { redirect } from 'next/navigation'
+import { SEO } from '@components/common/seo'
+import { LoginMain } from '@components/login/login-main'
+import { LoginFooter } from '@components/login/login-footer'
+import { AuthLayout } from '@components/layout/auth-layout'
 
 redirect('/home')
-function app() {
+export default function Login(): JSX.Element {
   return (
-    <div />
+    <div className="grid min-h-screen grid-rows-[1fr,auto]">
+      <SEO
+        title="Twitter - It’s what’s happening"
+        description="From breaking news and entertainment to sports and politics, get the full story with all the live commentary."
+      />
+      <LoginMain />
+      <LoginFooter />
+    </div>
   )
 }
-export default app
---
+++ apps/web/src/lib/context/window-context.tsx
@@ -0,0 +1,55 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import type { ReactNode } from 'react'
+
+interface WindowSize {
+  width: number
+  height: number
+}
+
+type TWindowContext = WindowSize & {
+  isMobile: boolean
+}
+
+export const WindowContext = createContext<TWindowContext | null>(null)
+
+interface WindowContextProviderProps {
+  children: ReactNode
+}
+
+export function WindowContextProvider({
+  children,
+}: WindowContextProviderProps): JSX.Element {
+  const [windowSize, setWindowSize] = useState<WindowSize>({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  })
+
+  useEffect(() => {
+    const handleResize = (): void =>
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      })
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  const value: TWindowContext = {
+    ...windowSize,
+    isMobile: windowSize.width < 500,
+  }
+
+  return (
+    <WindowContext.Provider value={value}>{children}</WindowContext.Provider>
+  )
+}
+
+export function useWindow(): TWindowContext {
+  const context = useContext(WindowContext)
+
+  if (!context)
+    throw new Error('useWindow must be used within an WindowContextProvider')
+
+  return context
+}
